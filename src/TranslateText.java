bd6541af32818013638ffb76a8a51f6d726bea93
Velislav Tsenov <velislav.tsenov@gmail.com>
2017-01-23 09:02:37 +0200

    Changed the translation algorithm to translate whole sentences.

--- src/TranslateText.java
+++ src/TranslateText.java
@@ -7,51 +7,51 @@ import java.io.IOException;
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.lang.Object;
 
 public class TranslateText {
 
 	public static int LINE_NUM = 0;
 
-	public static void main(String[] args) throws FileNotFoundException {
-
-		File file = new File("C:\\Users\\velis\\Desktop\\EnglishWords.txt");
-		File translatedFile = new File("C:\\Users\\velis\\Desktop\\BulgarianWords.txt");
-		Scanner inputTxt = new Scanner(System.in);
-		String[] inputText = inputTxt.nextLine().split("\\s");
-
-		ArrayList<String> list = (ArrayList<String>) Arrays.asList(inputText);
-		int currentPosition = 0;
-		
+	public static void main(String[] args) {
 		try {
-		
-			Scanner input = new Scanner(file);
-			Scanner user = new Scanner(System.in);
-			String line = null;
-			String userWord = user.next();
-			while (input.hasNextLine()) {
-				line = input.nextLine();
-				LINE_NUM++;
-				if (userWord.equals(line)) {
-					break;
-				}
-				
-			}
-				
+			File englishWords = new File("C:\\Users\\velis\\Desktop\\EnglishWords.txt");
+
+			Scanner readEnglishWords = new Scanner(englishWords);
+			Scanner userInput = new Scanner(System.in);
+			String userInputSentence = userInput.nextLine();
+			String[] userInputArray = userInputSentence.split(" ");
+			String currentEnglishWord = null;
+			String currentWord = " ";
+			String searchedWord = " " ;
+
 			int currentLine = 0;
+			File bulgarianWords = new File("C:\\Users\\velis\\Desktop\\BulgarianWords.txt");
+			Scanner readBulgarianWords = new Scanner(bulgarianWords);
 			String translatedWord = null;
-			Scanner findRow = new Scanner(translatedFile);
-			
-			while(LINE_NUM != currentLine) {
-				translatedWord = findRow.nextLine();
-				currentLine++;
-				if(LINE_NUM == currentLine) {
-					System.out.println(translatedWord);
-					break;
+
+			List<String> userSentence = new ArrayList<String>(Arrays.asList(userInputSentence.split(" ")));
+
+			while (!userSentence.equals(" ")) {
+				while (readEnglishWords.hasNextLine()) {
+					currentWord = readEnglishWords.nextLine();
+					LINE_NUM++;
+					if (searchedWord.equals(currentWord)) {
+						userSentence.remove(0);
+					}
+				}
+
+				while (LINE_NUM != currentLine) {
+					translatedWord = readBulgarianWords.nextLine();
+					currentLine++;
+					if (LINE_NUM == currentLine) {
+						System.out.println(translatedWord);
+					}
 				}
 			}
-			
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
