602e795f7b806ca30d7f197ee128f21eee3a0b7f
Velislav Tsenov <velislav.tsenov@gmail.com>
2017-01-27 10:30:27 +0200

    Fixed a bug where the translator won't return the inital word if not found.

--- src/TranslatorUI.java
+++ src/TranslatorUI.java
@@ -221,7 +221,7 @@ public class TranslatorUI {
 					// if the loop finds no matches throughout the document it
 					// returns the word
 
-					if (currentLine == MAX_ROWS + 2) {
+					if (currentLine == MAX_ROWS) {
 						translatedSentence.add(userInputSentence.get(0));
 						break;
 					}
